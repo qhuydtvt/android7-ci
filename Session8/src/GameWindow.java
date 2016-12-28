b915d0b2f38380c7d6792f30074cc5d58ef27117
qhuydtvt <qhuydtvt@gmail.com>
2016-12-28 20:34:14 +0700

    Add bomb explosion using Observer

---
+++ Session8/src/GameWindow.java
@@ -0,0 +1,152 @@
+import controllers.*;
+import controllers.manangers.BodyManager;
+import controllers.manangers.ControllerManager;
+import controllers.manangers.EnemyControllerManager;
+
+import java.awt.*;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.awt.image.BufferedImage;
+import java.util.Vector;
+
+import static utils.Utils.loadImage;
+
+// Data abstraction
+
+/**
+ * Created by apple on 11/30/16.
+ */
+public class GameWindow extends Frame implements Runnable {
+    Image background;
+
+    BufferedImage backBuffer;
+    GameSetting gameSetting;
+
+    Vector<BaseController> controllers;
+
+    public GameWindow() {
+
+        configSettings();
+
+        controllers = new Vector<>();
+        controllers.add(ControllerManager.explosion);
+        controllers.add(new EnemyControllerManager());
+        controllers.add(PlaneController.instance);
+        controllers.add(BodyManager.instance);
+        controllers.add(ControllerManager.enemyBullet);
+        controllers.add(ControllerManager.instance);
+
+        setVisible(true);
+        setSize(gameSetting.getWidth(), gameSetting.getHeight());
+
+        backBuffer = new BufferedImage(gameSetting.getWidth(), gameSetting.getHeight(), BufferedImage.TYPE_INT_ARGB);
+
+        addWindowListener(new WindowListener() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+                System.out.println("windowOpened");
+            }
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.out.println("windowClosing");
+                System.exit(0);
+            }
+
+            @Override
+            public void windowClosed(WindowEvent e) {
+                System.out.println("windowClosed");
+
+            }
+
+            @Override
+            public void windowIconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeiconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowActivated(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+
+            }
+        });
+        background = loadImage("resources/background.png");
+
+        addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                System.out.println( " keyTyped");
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                System.out.println("keyPressed");
+                PlaneController.instance.keyPressed(e);
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                System.out.println("keyReleased");
+            }
+        });
+    }
+
+    private void configSettings() {
+        PlaneController.instance.keySetting = new KeySetting(
+                KeyEvent.VK_UP,
+                KeyEvent.VK_DOWN,
+                KeyEvent.VK_LEFT,
+                KeyEvent.VK_RIGHT,
+                KeyEvent.VK_SPACE
+        );
+        gameSetting = GameSetting.instance;
+    }
+
+
+    //Utilities
+
+
+    @Override
+    public void update(Graphics g) {
+        // Prepare backbuffer
+        Graphics backBufferGraphics = backBuffer.getGraphics();
+        backBufferGraphics.drawImage(background, 0, 0, gameSetting.getWidth(), gameSetting.getHeight(), null);
+
+        for (BaseController baseController : this.controllers) {
+            baseController.draw(backBufferGraphics);
+        }
+
+        // Update window
+        g.drawImage(backBuffer, 0, 0, gameSetting.getWidth(), gameSetting.getHeight(), null);
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                this.repaint();
+                Thread.sleep(17);
+
+                for(BaseController baseController: controllers) {
+                    baseController.run();
+                }
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
